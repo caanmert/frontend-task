42b165df484957120fc21dae33c9a3acb028d999
Can Mert <canmert.dev@gmail.com>
2022-05-06 12:03:32 +0300

    adding svg icons for register form, propChecking for Icon component and removing InputField component

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,8 @@
 import Button from './Button/Button';
 import FormField from './FormField/FormField';
 import Icon from './Icon/Icon';
-import InputField from './InputField/InputField';
 import UserList from './UserList/UserList';
 
 export {
-  Button, Icon, InputField, UserList, FormField,
+  Button, Icon, UserList, FormField,
 };
