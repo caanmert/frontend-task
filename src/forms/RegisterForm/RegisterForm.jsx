08134410db9606c3bce197cede540da095eabf52
canmert <canmert.dev@gmail.com>
2022-05-08 19:45:35 +0300

    seperating bottom section of register form

--- src/forms/RegisterForm/RegisterForm.jsx
+++ src/forms/RegisterForm/RegisterForm.jsx
@@ -3,9 +3,10 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './registerForm.css';
 import { Button, FormField } from '../../components';
-import { registerFormInputs, registerFormIcons } from '../../constants/RegisterFormConstants';
+import { inputs } from '../../contents/registerForm';
 import { formatDate } from '../../service/DateService';
 import { ReactComponent as GmailIcon } from '../../assets/gmail-icon.svg';
+import BottomSection from './BottomSection';
 
 const initialState = {
   firstname: '',
@@ -15,7 +16,7 @@ const initialState = {
   createdAt: formatDate(new Date()),
 };
 
-function SignUpForm({ handleSubmit }) {
+function RegisterForm({ handleSubmit }) {
   const [values, setValues] = useState(initialState);
   const [isSubmitted, setSubmitted] = useState(false);
 
@@ -38,53 +39,38 @@ function SignUpForm({ handleSubmit }) {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
 
+  const formList = inputs.map((input) => (
+    <div className="form-field-container" key={input.id}>
+      <FormField
+        key={input.id}
+        type={input.type}
+        {...input}
+        value={values[input.name]}
+        onChange={onChange}
+        shouldFieldReset={isSubmitted}
+      />
+    </div>
+  ));
+
   return (
     <>
       <div className="transparent-div" />
       <div className="register-form">
-        {/*     <i className="fa-solid fa-envelope top-icon" /> */}
         <GmailIcon className="top-icon" />
         <div className="register-form-fields">
           <h3><strong>Sign up</strong></h3>
-          <form onSubmit={(e) => onSubmitBtnClick(e, values)}>
-            {registerFormInputs.map((input) => (
-              <div className="form-field-container" key={input.id}>
-                <FormField
-                  key={input.id}
-                  type={input.type}
-                  {...input}
-                  value={values[input.name]}
-                  onChange={onChange}
-                  shouldFieldReset={isSubmitted}
-                />
-              </div>
-            ))}
+          <form onSubmit={(e) => onSubmitBtnClick(e)}>
+            {formList}
             <div className="terms-container">
               <label htmlFor="terms">
                 <input className="terms-checkbox" type="checkbox" id="terms" name="terms" required />
                 <small> I agree with terms and conditions</small>
               </label>
             </div>
-            <div style={{ textAlign: 'center' }}>
-              <Button type="submit" onPress={handleSubmit}>
-                Sign up
-              </Button>
-              <p><small>sponsored by</small></p>
-              <div className="bottom-icons-container">
-                {registerFormIcons.map((icon) => (
-                  <div className="bottom-icon" key={icon.id}>
-                    <img src={icon.url} alt={icon.alt} style={{ maxHeight: '70%', objectFit: 'contain' }} />
-                    {/*   <Icon fontAwesomeIconName={icon.name} /> */}
-                  </div>
-                ))}
-              </div>
-              <p>
-                <small>
-                  Already have an account?
-                  <a href="link" style={{ color: 'green', fontWeight: 'bold' }}>Sign In</a>
-                </small>
-              </p>
-            </div>
+            <Button type="submit" onPress={handleSubmit}>
+              Sign up
+            </Button>
+            <BottomSection />
           </form>
         </div>
       </div>
@@ -92,8 +78,8 @@ function SignUpForm({ handleSubmit }) {
   );
 }
 
-SignUpForm.propTypes = {
+RegisterForm.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
 };
 
-export default SignUpForm;
+export default RegisterForm;
